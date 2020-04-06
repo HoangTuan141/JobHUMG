6bfd81105ba2874cc4c897ebaa197eead19d98a2
TuanHA <tuanha3@rikkeisoft.com>
2020-04-06 08:36:01 +0700

    add Placeholder textView

--- jobHUMG/jobHUMG/Controllers/PostJobVC/PostJobVC.swift
+++ jobHUMG/jobHUMG/Controllers/PostJobVC/PostJobVC.swift
@@ -9,14 +9,34 @@
 import UIKit
 
 class PostJobVC: UIViewController {
-
+    @IBOutlet weak var careerTextField: UITextField!
+    @IBOutlet weak var companyTextField: UITextField!
+    @IBOutlet weak var addressTextView: PlaceholderTextView!
+    @IBOutlet weak var descriptionTextView: PlaceholderTextView!
+    @IBOutlet weak var postButton: UIButton!
+    @IBOutlet weak var heightAddressTextView: NSLayoutConstraint!
+    @IBOutlet weak var heightDescriptionTextView: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupView()
+        
     }
+    
+    private func setupView() {
+        addressTextView.placeholder = "Nhập địa chỉ công ty"
+        addressTextView.padding = UIEdgeInsets(top: 14, left: 14, bottom: 14, right: 14)
+        addressTextView.heightDidChange = { [weak self] height in
+            self?.heightAddressTextView.constant = height > 44 ? height : 44
+    }
+    }
+    
     @IBAction func backPressed(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func postPressed(_ sender: Any) {
+        
     }
     
 }
